12696913e5743cc9267ce05e80a069815c64bf27
Mayukh Choudhury <Mayukh Choudhury@DESKTOP-1O3C6MO>
2022-05-23 23:36:05 +0530

    initial push

---
+++ SpringBoot-Email-Client/src/test/java/com/techsaby/SpringBootEMailClient/SpringBootEMailClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsaby.SpringBootEMailClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEMailClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
